09337adc0a8189173ece09e38cc1787f225a4a6c
tasconajoey <joey.tascona@gmail.com>
2017-02-15 22:24:38 -0500

    Not really well done but kinda tic-tac-toe

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
-function getValue(){
-   var retVal = prompt("X or O : ", "");
-   document.write("You have entered : " + retVal);
-}
+// function getValue() {
+   // var retVal = prompt("x or o : ", "???");
+   // if (retVal === "x") {
+   //    $('.grid').text('x');
+   // } else {
+   //    $('.grid').text('o');
+   // }
+// }
+$('.grid').on('click', function() {
+   var retVal = prompt("x or o : ", "???");
+   if (retVal === "x") {
+      $(this).text('x');
+   } else {
+      $(this).text('o');
+   }
+})
